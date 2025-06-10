54b5114bcb702a95505424a4cd8a4489985c49c3
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-06-10 13:50:05 +0300

    Configured Hardhat for Arbitrum mainnet and testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,16 @@
 
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle')
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
+  const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address)
+    console.log(account.address);
   }
-})
+});
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: '0.8.6',
   settings: {
@@ -25,5 +18,21 @@ module.exports = {
       enabled: true,
       runs: 200
     }
+  },
+  networks: {
+    arbitrumTestnet: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 421614,
+    },
+    arbitrumMainnet: {
+      url: process.env.ARBITRUM_MAINNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: process.env.ETHERSCAN_API_KEY
+    }
   }
-}
+};
