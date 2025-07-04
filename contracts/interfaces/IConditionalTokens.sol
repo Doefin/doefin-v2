c02fa6549a7656af341f24e589d4382897d71220
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-07-04 16:17:45 +0300

    Adjust the interfaces events, added interfaces for semi fungible tokens

--- contracts/interfaces/IConditionalTokens.sol
+++ contracts/interfaces/IConditionalTokens.sol
@@ -2,17 +2,48 @@
 pragma solidity ^0.8.6;
 
 interface IConditionalTokens {
-    event ConditionPrepared(bytes32 indexed conditionId, address indexed oracle, bytes32 indexed questionId);
-    event PositionSplit(address indexed user, bytes32 indexed conditionId, uint amount);
-    event PositionMerged(address indexed user, bytes32 indexed conditionId, uint amount);
-    event PositionRedeemed(address indexed user, bytes32 indexed conditionId, uint amount);
+    event ConditionPreparation(bytes32 indexed conditionId, address indexed oracle, bytes32 indexed questionId, uint outcomeSlotCount);
+
+    event PositionSplit(
+        address indexed stakeholder,
+        address indexed collateralToken,
+        bytes32 indexed parentCollectionId,
+        bytes32 conditionId,
+        uint[] partition,
+        uint amount
+    );
+
+    event ConditionResolution(
+        bytes32 indexed conditionId,
+        address indexed oracle,
+        bytes32 indexed questionId,
+        uint outcomeSlotCount,
+        uint[] payoutNumerators
+    );
+
+    event PositionsMerge(
+        address indexed stakeholder,
+        address indexed collateralToken,
+        bytes32 indexed parentCollectionId,
+        bytes32 conditionId,
+        uint[] partition,
+        uint amount
+    );
+
+    event PayoutRedemption(
+        address indexed redeemer,
+        address indexed collateralToken,
+        bytes32 indexed parentCollectionId,
+        bytes32 conditionId,
+        uint[] indexSets,
+        uint payout
+    );
 
     function prepareCondition(address oracle, bytes32 questionId, uint outcomeSlotCount) external;
     function reportPayouts(bytes32 questionId, uint[] calldata payouts) external;
     function splitPosition(address collateralToken, bytes32 parentCollectionId, bytes32 conditionId, uint amount, uint[] calldata partition) external;
-    function mergePositions(address collateralToken, bytes32 parentCollectionId, bytes32 conditionId, uint amount) external;
+    function mergePositions(address collateralToken, bytes32 parentCollectionId, bytes32 conditionId, uint256[] calldata partition, uint256 amount) external;
     function redeemPositions(address collateralToken, bytes32 parentCollectionId, bytes32 conditionId, uint[] calldata indexSets) external;
     function getPositionId(address collateralToken, bytes32 collectionId, uint indexSet) external view returns (uint);
     function getPayoutNumerators(bytes32 conditionId) external view returns (uint[] memory);
-    function getPositionTokenAddress(uint positionId) external view returns (address);
 }
---
+++ contracts/interfaces/IERC1155TokenReceiver.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+/**
+ * @title IERC1155TokenReceiver
+ * @dev Interface for contracts that want to handle safe receipt of ERC1155 tokens.
+ *      See https://eips.ethereum.org/EIPS/eip-1155
+ */
+interface IERC1155TokenReceiver {
+    /**
+     * @notice Handle the receipt of a single ERC1155 token type.
+     * @dev Must return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+     *      (i.e. `0xf23a6e61`) to accept the transfer.
+     * @param operator The address that initiated the transfer (i.e. msg.sender)
+     * @param from The address which previously owned the token
+     * @param id The ID of the token being transferred
+     * @param value The amount of tokens being transferred
+     * @param data Additional data with no specified format
+     * @return The selector to confirm the token transfer acceptance
+     */
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4);
+
+    /**
+     * @notice Handle the receipt of multiple ERC1155 token types.
+     * @dev Must return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
+     *      (i.e. `0xbc197c81`) to accept the transfer.
+     * @param operator The address that initiated the batch transfer (i.e. msg.sender)
+     * @param from The address which previously owned the token
+     * @param ids The IDs of each token being transferred
+     * @param values The amounts of each token being transferred
+     * @param data Additional data with no specified format
+     * @return The selector to confirm the batch transfer acceptance
+     */
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external returns (bytes4);
+}
