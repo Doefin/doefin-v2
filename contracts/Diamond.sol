73e11003a75344e8344c686ebb7f05cb14f4f008
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-07-04 16:24:32 +0300

    Added the new facets to the initializer contract

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -9,12 +9,14 @@ pragma solidity ^0.8.0;
 /******************************************************************************/
 
 import { LibDiamond } from "./libraries/LibDiamond.sol";
+import { LibAccessControl } from "./libraries/LibAccessControl.sol";
 import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
 
 contract Diamond {    
 
     constructor(address _contractOwner, address _diamondCutFacet) payable {        
         LibDiamond.setContractOwner(_contractOwner);
+        LibAccessControl.setOwner(_contractOwner);
 
         // Add the diamondCut external function from the diamondCutFacet
         IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
