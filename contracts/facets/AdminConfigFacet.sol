1f49314e86192abd629b4222bce22a9dfbdc6680
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-07-09 19:12:05 +0300

    Admin config facet and interface added to allow configuring fees and collateral types

---
+++ contracts/facets/AdminConfigFacet.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import {LibDoefinStorage} from "../libraries/LibDoefinStorage.sol";
+import {IAdminConfig} from "../interfaces/IAdminConfig.sol";
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+
+contract AdminConfigFacet is IAdminConfig {
+    function addCollateralToken(address token, uint256 unitPerPair) external override {
+        LibDiamond.enforceIsContractOwner();
+        require(token != address(0), "AdminConfig: invalid token address");
+        require(unitPerPair > 0, "AdminConfig: unit must be > 0");
+
+        LibDoefinStorage.DiamondStorage storage ds = LibDoefinStorage.diamondStorage();
+
+        require(!ds.adminConfigStorage.isAllowed[token], "AdminConfig: token already allowed");
+
+        ds.adminConfigStorage.isAllowed[token] = true;
+        ds.adminConfigStorage.unitPerPair[token] = unitPerPair;
+
+        emit CollateralTokenAdded(token, unitPerPair);
+    }
+
+    function removeCollateralToken(address token) external override {
+        LibDiamond.enforceIsContractOwner();
+        LibDoefinStorage.DiamondStorage storage ds = LibDoefinStorage.diamondStorage();
+
+        require(ds.adminConfigStorage.isAllowed[token], "AdminConfig: token not allowed");
+
+        ds.adminConfigStorage.isAllowed[token] = false;
+        delete ds.adminConfigStorage.unitPerPair[token];
+
+        emit CollateralTokenRemoved(token);
+    }
+
+    function setFeeReceiver(address feeReceiver) external override {
+        LibDiamond.enforceIsContractOwner();
+        require(feeReceiver != address(0), "AdminConfig: invalid fee receiver");
+        LibDoefinStorage.diamondStorage().adminConfigStorage.feeReceiver = feeReceiver;
+        emit FeeReceiverUpdated(feeReceiver);
+    }
+
+    function setResolutionFeeBps(uint256 bps) external override {
+        LibDiamond.enforceIsContractOwner();
+        require(bps <= 10_000, "AdminConfig: fee too high");
+
+        LibDoefinStorage.diamondStorage().adminConfigStorage.resolutionFeeBps = bps;
+        emit ResolutionFeeUpdated(bps);
+    }
+
+    function setTradingFeesBps(uint256 makerBps, uint256 takerBps) external override {
+        LibDiamond.enforceIsContractOwner();
+        require(makerBps <= 10_000 && takerBps <= 10_000, "AdminConfig: fee too high");
+
+        LibDoefinStorage.DiamondStorage storage ds = LibDoefinStorage.diamondStorage();
+        ds.adminConfigStorage.makerTradingFeeBps = makerBps;
+        ds.adminConfigStorage.takerTradingFeeBps = takerBps;
+
+        emit TradingFeesUpdated(makerBps, takerBps);
+    }
+
+    function isAllowedCollateral(address token) external view override returns (bool) {
+        return LibDoefinStorage.diamondStorage().adminConfigStorage.isAllowed[token];
+    }
+
+    function getCollateralUnit(address token) external view override returns (uint256) {
+        require(LibDoefinStorage.diamondStorage().adminConfigStorage.isAllowed[token], "AdminConfig: token not allowed");
+        return LibDoefinStorage.diamondStorage().adminConfigStorage.unitPerPair[token];
+    }
+
+    function getFees() external view returns (address feeReceiver, uint256 resolutionFeeBps, uint256 makerTradingFeeBps, uint256 takerTradingFeeBps) {
+        LibDoefinStorage.AdminConfigStorage storage cfg = LibDoefinStorage.diamondStorage().adminConfigStorage;
+        return (cfg.feeReceiver, cfg.resolutionFeeBps, cfg.makerTradingFeeBps, cfg.takerTradingFeeBps);
+    }
+}
---
+++ contracts/interfaces/IAdminConfig.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+interface IAdminConfig {
+    event CollateralTokenAdded(address token, uint256 unitPerPair);
+    event CollateralTokenRemoved(address token);
+    event FeeReceiverUpdated(address newReceiver);
+    event ResolutionFeeUpdated(uint256 newBps);
+    event TradingFeesUpdated(uint256 makerBps, uint256 takerBps);
+
+    function addCollateralToken(address token, uint256 unitPerPair) external;
+    function removeCollateralToken(address token) external;
+    function isAllowedCollateral(address token) external view returns (bool);
+    function getCollateralUnit(address token) external view returns (uint256);
+    function setFeeReceiver(address feeReceiver) external;
+    function setResolutionFeeBps(uint256 bps) external;
+    function setTradingFeesBps(uint256 makerBps, uint256 takerBps) external;
+}
