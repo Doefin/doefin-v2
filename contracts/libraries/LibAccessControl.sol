916e687bd9cb5d5f8a634c462427dd4f1edf2398
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-07-09 19:26:54 +0300

    Small refactoring to set market maker and isOwner method

--- contracts/libraries/LibAccessControl.sol
+++ contracts/libraries/LibAccessControl.sol
@@ -6,8 +6,8 @@ import {LibDiamond} from "../libraries/LibDiamond.sol";
 
 library LibAccessControl {
     // Owner
-    function enforceIsOwner() internal view {
-        require(msg.sender == LibDiamond.contractOwner(), "AccessControl: must be owner");
+    function isOnwer(address _account) internal view returns (bool) {
+        return _account == LibDiamond.contractOwner();
     }
 
     // MarketMaker
@@ -20,7 +20,7 @@ library LibAccessControl {
     }
 
     function setMarketMaker(address _account, bool _status) internal {
-        enforceIsOwner();
+        require(isOnwer(msg.sender), "AccessControl: must be owner");
         LibDoefinStorage.diamondStorage().accessControl.marketMakers[_account] = _status;
     }
 }
