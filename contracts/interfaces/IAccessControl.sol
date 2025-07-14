09bc69f26e212eb1b5f2aec77aaec3ae2bb83475
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-07-14 14:52:06 +0300

    Update all licenses to AGPL and also mentiond Both gnosis and diamond pattern

--- contracts/interfaces/IAccessControl.sol
+++ contracts/interfaces/IAccessControl.sol
@@ -1,4 +1,7 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0
+// Based on Diamond Standard by Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+// Uses shared logic from Gnosis Conditional Tokens Framework: https://github.com/gnosis/conditional-tokens-contracts
+
 pragma solidity ^0.8.6;
 
 interface IAccessControl {
--- contracts/interfaces/IAdminConfig.sol
+++ contracts/interfaces/IAdminConfig.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
-// Uses shared storage derived from Gnosis Conditional Tokens Framework: https://github.com/gnosis/conditional-tokens-contracts
+// Based on Diamond Standard by Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+// Uses shared logic from Gnosis Conditional Tokens Framework: https://github.com/gnosis/conditional-tokens-contracts
+
 
 pragma solidity ^0.8.6;
 
