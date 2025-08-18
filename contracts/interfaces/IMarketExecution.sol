a3215821bd0d5fd34a1af49309f1f079730b4527
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-08-18 14:57:18 +0300

    Implemented required methods for exeucting market orders and filling limit orders

---
+++ contracts/interfaces/IMarketExecution.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0
+// Based on Diamond Standard by Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+// Uses shared logic from Gnosis Conditional Tokens Framework: https://github.com/gnosis/conditional-tokens-contracts
+
+pragma solidity ^0.8.6;
+
+import {LibDoefinStorage} from "../libraries/LibDoefinStorage.sol";
+
+interface IMarketExecution {
+    /// @notice Fill a market order using a precomputed match route
+    /// @dev Caller must provide a route returned from simulateMarketOrder, and enforce slippage
+    function fillMarketOrderWithRoute(
+        uint256 positionId,
+        uint256 amount,
+        uint256 targetAvgPrice,
+        bool fillOrKill,
+        LibDoefinStorage.OrderDirection direction,
+        LibDoefinStorage.MatchExecution[] calldata matches
+    ) external;
+
+    function fillLimitOrders(uint256 takerId, uint256[] calldata makerIds) external;
+}
