7396234a340f4097707cc54a504156d2b90744b5
Reza Jahankohan <r.jahankohan@gmail.com>
2025-11-27 14:45:58 +0300

    Added the required data structure and storage for the BTC Oracle

--- contracts/libraries/LibDoefinStorage.sol
+++ contracts/libraries/LibDoefinStorage.sol
@@ -7,6 +7,11 @@ library LibDoefinStorage {
     // Add initialization flag
     bytes32 constant INITIALIZED_POSITION = keccak256("doefin.storage.initialized");
 
+    // Block Header Oracle constants
+    uint256 constant SETTLEMENT_DELAY = 6;
+    uint256 constant NUM_OF_TIMESTAMPS = 11;
+    uint256 constant NUM_OF_BLOCK_HEADERS = 17;
+
     struct ERC1155Storage {
         mapping(uint256 => mapping(address => uint256)) erc1155Balances;
         mapping(address => mapping(address => bool)) erc1155OperatorApprovals;
@@ -220,6 +225,43 @@ library LibDoefinStorage {
         uint256[10] __gap;
     }
 
+    /**
+     * @dev Struct to store essential details of a block header
+     * @param version The block version number. Indicates which set of block validation rules to follow. Miners
+     * increment this number and also use it in combination with the nonce when searching for a valid block hash.
+     * @param prevBlockHash A 256-bit hash of the previous block’s header. Ensures that blocks are ordered
+     * chronologically and establishes the link between blocks in the blockchain.
+     * @param merkleRootHash A 256-bit hash derived from the transactions in the block. Provides a single hash that
+     * summarizes all transactions in the block, enabling efficient and secure verification of the block’s contents.
+     * @param timestamp The approximate creation time of the block, represented as Unix time (seconds since
+     * 1970-01-01T00:00 UTC). Provides a temporal ordering of blocks and helps in the difficulty adjustment process.
+     * @param nBits Encoded current target threshold for the proof-of-work algorithm. Represents the difficulty target
+     * that a block’s hash must meet. It is periodically adjusted to maintain the block creation rate.
+     * @param nonce A 32-bit arbitrary number that miners adjust to find a hash below the target threshold. Used by
+     * miners in the proof-of-work algorithm. Miners increment the nonce to generate different hashes and find a valid
+     * one that meets the difficulty target.
+     */
+    struct BlockHeader {
+        uint32 version;
+        bytes32 prevBlockHash;
+        bytes32 merkleRootHash;
+        uint32 timestamp;
+        uint32 nBits;
+        uint32 nonce;
+        bytes32 blockHash;
+        uint256 blockNumber;
+    }
+
+    struct BlockHeaderOracleStorage {
+        // @notice Store the block number separately since the block number is not part of the block header information.
+        uint256 currentBlockHeight;
+        /// @notice Track the index of the next block in the ring buffer
+        uint256 nextBlockIndex;
+        /// @notice Ring buffer to store the block headers
+        BlockHeader[NUM_OF_BLOCK_HEADERS] blockHeaders;
+        uint256[10] __gap;
+    }
+
     struct ReentrancyStorage {
         uint256 _status;
         uint256[10] __gap;
@@ -234,6 +276,7 @@ library LibDoefinStorage {
         EscrowStorage escrowStorage;
         PositionRegistryStorage positionRegistry;
         ReentrancyStorage reentrancyStorage;
+        BlockHeaderOracleStorage blockHeaderOracleStorage;
         uint256[50] __gap;
     }
 
