3c00a4936c447a308856a0b1978058b644916da1
Reza Jahankohan <r.jahankohan@gmail.com>
2025-11-27 14:45:58 +0300

    Added a new endpoint for creating conditions with metadata

--- contracts/interfaces/IConditionManager.sol
+++ contracts/interfaces/IConditionManager.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 // Based on Diamond Standard by Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
 // Uses shared logic from Gnosis Conditional Tokens Framework: https://github.com/gnosis/conditional-tokens-contracts
+
 import {LibDoefinStorage} from "../libraries/LibDoefinStorage.sol";
 
 pragma solidity ^0.8.6;
@@ -12,6 +13,14 @@ interface IConditionManager {
         uint8 outcomeSlotCount,
         string calldata metadataURI
     ) external returns (bytes32 conditionId);
+
+    function createConditionWithMetadata(
+        LibDoefinStorage.QuestionType questionType,
+        bytes calldata metadata,
+        uint8 outcomeSlotCount,
+        string calldata metadataURI,
+        bytes32 salt
+    ) external returns (bytes32 conditionId, bytes32 questionId);
 
     function getCondition(bytes32 conditionId)
         external
