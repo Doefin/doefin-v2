d043922f95c8155607c96cfd953049db9ccb77a2
Reza Jahankohan <r.jahankohan@gmail.com>
2026-04-07 01:28:31 +0300

    Settlement endopints interfaces defined

---
+++ contracts/interfaces/ISettlement.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.6;
+
+import {LibDoefinOrder} from "../libraries/LibDoefinOrder.sol";
+
+/**
+ * @title ISettlement
+ * @author Doefin
+ * @notice Interface for the v2.1 hybrid settlement facet
+ * @dev Operator-only entry point for executing matched order pairs on-chain
+ */
+interface ISettlement {
+    function matchOrders(
+        LibDoefinOrder.DoefinOrder calldata takerOrder,
+        bytes calldata takerSignature,
+        uint8 takerSignatureType,
+        LibDoefinOrder.DoefinOrder[] calldata makerOrders,
+        bytes[] calldata makerSignatures,
+        uint8[] calldata makerSignatureTypes,
+        uint128 takerFillAmount,
+        uint128[] calldata makerFillAmounts
+    ) external;
+
+    function fillOrder(
+        LibDoefinOrder.DoefinOrder calldata order,
+        bytes calldata signature,
+        uint8 signatureType,
+        uint128 fillAmount
+    ) external;
+
+    function setOperator(address _operator) external;
+
+    function pauseTrading() external;
+
+    function unpauseTrading() external;
+
+    function getFilledAmount(bytes32 orderHash) external view returns (uint256);
+
+    function getOperator() external view returns (address);
+
+    function isTradingPaused() external view returns (bool);
+}
